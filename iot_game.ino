dbfa83ab18c4a3c6775752373f2000aebd5c6bc2
Akash Manohar J <akash@akash.im>
2014-11-22 23:45:46 +0530

    Add led pin 13 as indicator

--- iot_game.ino
+++ iot_game.ino
@@ -1,19 +1,24 @@
 int JUMP_PIN = 2;
+int LED_PIN  = 13;
 int result;
 
 void setup() {
   Serial.begin(9600);
   pinMode(JUMP_PIN, INPUT_PULLUP);
+  pinMode(LED_PIN,  OUTPUT);
 }
 
 
 void loop () {
   int jump_status = digitalRead(JUMP_PIN);
-  if (jump_status)
-  result = 1; // high - jumped
-  else
-  result = 0; // low - not jumped
+  if (jump_status) {
+    result = 1; // high - jumped
+    digitalWrite(LED_PIN, LOW);
+  }
+  else {
+    result = 0; // low - not jumped
+    digitalWrite(LED_PIN, HIGH);
+  }
 
   Serial.println(result);
-  delay(1000);
 }
