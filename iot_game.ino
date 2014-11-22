5c7e3db0065f62946f84b9ac90b12a69df3b0bc2
Akash Manohar J <akash@akash.im>
2014-11-23 00:30:07 +0530

    Just formatting to feed my OCD

--- iot_game.ino
+++ iot_game.ino
@@ -11,12 +11,13 @@ void setup() {
 
 void loop () {
   int jump_status = digitalRead(JUMP_PIN);
+
   if (jump_status) {
-    digitalWrite(LED_PIN, LOW);
     // low - not jumped
-  }
-  else {
-    Serial.println(1); // high - jumped
+    digitalWrite(LED_PIN, LOW);
+  } else {
+    // high - jumped
+    Serial.println(1);
     digitalWrite(LED_PIN, HIGH);
   }
   delay(400);
