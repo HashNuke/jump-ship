67e4290e6346908d6fc77216ae01363bce877b68
Akash Manohar J <akash@akash.im>
2014-11-23 06:35:59 +0530

    Simplify serial stuff; Get rid of the example used

--- game.js
+++ game.js
@@ -22,109 +22,111 @@ $(document).ready(function(){
 
 
   var startGame = function() {
-    console.log("start called");
+    console.log("Starting game...");
 
-    const DEVICE_PATH = $(".device-list").val();
-    const serial = chrome.serial;
+    var device_path = $(".device-list").val(),
+        serial = chrome.serial,
+        lineBuffer = "",
+        serialConnId = -1,
+        onSerialConnect = new chrome.Event(),
+        onSerialReadLine = new chrome.Event(),
+        onSerialError = new chrome.Event();
 
-    /* Interprets an ArrayBuffer as UTF-8 encoded string data. */
-    var ab2str = function(buf) {
-      var bufView = new Uint8Array(buf);
-      var encodedString = String.fromCharCode.apply(null, bufView);
-      return decodeURIComponent(escape(encodedString));
-    };
+    var onConnect = function(conn) {
+      if(conn) {
+        serialConnId = conn.connectionId;
 
-    /* Converts a string to UTF-8 encoding in a Uint8Array; returns the array buffer. */
-    var str2ab = function(str) {
-      var encodedString = unescape(encodeURIComponent(str));
-      var bytes = new Uint8Array(encodedString.length);
-      for (var i = 0; i < encodedString.length; ++i) {
-        bytes[i] = encodedString.charCodeAt(i);
       }
-      return bytes.buffer;
+      else
+        alert("Something went wrong when connecting to serial device");
     };
 
 
-    ////////////////////////////////////////////////////////
-    ////////////////////////////////////////////////////////
-
-    var SerialConnection = function() {
-      this.connectionId = -1;
-      this.lineBuffer = "";
-      this.boundOnReceive = this.onReceive.bind(this);
-      this.boundOnReceiveError = this.onReceiveError.bind(this);
-      this.onConnect = new chrome.Event();
-      this.onReadLine = new chrome.Event();
-      this.onError = new chrome.Event();
-    };
-
-    SerialConnection.prototype.onConnectComplete = function(connectionInfo) {
-      if (!connectionInfo) {
-        log("Connection failed.");
-        return;
-      }
-      this.connectionId = connectionInfo.connectionId;
-      chrome.serial.onReceive.addListener(this.boundOnReceive);
-      chrome.serial.onReceiveError.addListener(this.boundOnReceiveError);
-      this.onConnect.dispatch();
-    };
-
-    SerialConnection.prototype.onReceive = function(receiveInfo) {
-      if (receiveInfo.connectionId !== this.connectionId) {
-        return;
-      }
-
-      this.lineBuffer += ab2str(receiveInfo.data);
-
-      var index;
-      while ((index = this.lineBuffer.indexOf('\n')) >= 0) {
-        var line = this.lineBuffer.substr(0, index + 1);
-        this.onReadLine.dispatch(line);
-        this.lineBuffer = this.lineBuffer.substr(index + 1);
-      }
-
-    };
-
-    SerialConnection.prototype.onReceiveError = function(errorInfo) {
-      if (errorInfo.connectionId === this.connectionId) {
-        this.onError.dispatch(errorInfo.error);
+    var onReceive = function(info) {
+      if (info.connectionId == serialConnId && info.data) {
+        // we got ping
+        console.log("we got a ping");
       }
     };
 
-    SerialConnection.prototype.connect = function(path) {
-      serial.connect(path, this.onConnectComplete.bind(this))
+
+    var flushSerialBuffer = function() {
+      chrome.serial.flush(serialConnId, function() {});
     };
 
-    SerialConnection.prototype.send = function(msg) {
-      if (this.connectionId < 0) {
-        throw 'Invalid connection';
-      }
-      serial.send(this.connectionId, str2ab(msg), function() {});
-    };
 
-    SerialConnection.prototype.disconnect = function() {
-      if (this.connectionId < 0) {
-        throw 'Invalid connection';
-      }
-      serial.disconnect(this.connectionId, function() {});
-    };
+    serial.onReceive.addListener(onReceive);
+    serial.connect(device_path, {bitrate: 115200}, onConnect);
+  };
 
-    ////////////////////////////////////////////////////////
-    ////////////////////////////////////////////////////////
-
-    var connection = new SerialConnection();
-
-    connection.onConnect.addListener(function() {
-      console.log('connected to: ' + DEVICE_PATH);
-      connection.send("hello arduino");
-    });
-
-    connection.onReadLine.addListener(function(line) {
-      console.log(line);
-    });
-
-    connection.connect(DEVICE_PATH);
-  }
+  //   SerialConnection.prototype.onConnectComplete = function(connectionInfo) {
+  //     if (!connectionInfo) {
+  //       log("Connection failed.");
+  //       return;
+  //     }
+  //
+  //     this.connectionId = connectionInfo.connectionId;
+  //     chrome.serial.onReceive.addListener(this.boundOnReceive);
+  //     chrome.serial.onReceiveError.addListener(this.boundOnReceiveError);
+  //     this.onConnect.dispatch();
+  //   };
+  //
+  //   SerialConnection.prototype.onReceive = function(receiveInfo) {
+  //     if (receiveInfo.connectionId !== this.connectionId) {
+  //       return;
+  //     }
+  //
+  //     this.lineBuffer += ab2str(receiveInfo.data);
+  //
+  //     var index;
+  //     while ((index = this.lineBuffer.indexOf('\n')) >= 0) {
+  //       var line = this.lineBuffer.substr(0, index + 1);
+  //       this.onReadLine.dispatch(line);
+  //       this.lineBuffer = this.lineBuffer.substr(index + 1);
+  //     }
+  //
+  //   };
+  //
+  //   SerialConnection.prototype.onReceiveError = function(errorInfo) {
+  //     if (errorInfo.connectionId === this.connectionId) {
+  //       this.onError.dispatch(errorInfo.error);
+  //     }
+  //   };
+  //
+  //   SerialConnection.prototype.connect = function(path) {
+  //     serial.connect(path, this.onConnectComplete.bind(this))
+  //   };
+  //
+  //   SerialConnection.prototype.send = function(msg) {
+  //     if (this.connectionId < 0) {
+  //       throw 'Invalid connection';
+  //     }
+  //     serial.send(this.connectionId, str2ab(msg), function() {});
+  //   };
+  //
+  //   SerialConnection.prototype.disconnect = function() {
+  //     if (this.connectionId < 0) {
+  //       throw 'Invalid connection';
+  //     }
+  //     serial.disconnect(this.connectionId, function() {});
+  //   };
+  //
+  //   ////////////////////////////////////////////////////////
+  //   ////////////////////////////////////////////////////////
+  //
+  //   var connection = new SerialConnection();
+  //
+  //   connection.onConnect.addListener(function() {
+  //     console.log('connected to: ' + DEVICE_PATH);
+  //     connection.send("hello arduino");
+  //   });
+  //
+  //   connection.onReadLine.addListener(function(line) {
+  //     console.log(line);
+  //   });
+  //
+  //   connection.connect(DEVICE_PATH);
+  // }
 
 
   $(".scan-devices").click(scanDevices);
